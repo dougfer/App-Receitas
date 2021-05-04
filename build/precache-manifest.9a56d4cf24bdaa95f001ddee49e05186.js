b1a9227667981ddac1ac6a13d74ed3fa90726a23
Douglas Ferreira <dodofp.df@gmail.com>
2021-05-04 16:41:42 -0300

    Run build

---
+++ build/precache-manifest.9a56d4cf24bdaa95f001ddee49e05186.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e579d1d0e4a03ddda9a01f6a0f3afcd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1609a63a7f18cdfd113f",
+    "url": "/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "18078cd416712a39c101",
+    "url": "/static/css/main.94b1c85a.chunk.css"
+  },
+  {
+    "revision": "1609a63a7f18cdfd113f",
+    "url": "/static/js/2.05cfd74f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.05cfd74f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18078cd416712a39c101",
+    "url": "/static/js/main.0f9b6583.chunk.js"
+  },
+  {
+    "revision": "3d4965c9977dafc48a8e",
+    "url": "/static/js/runtime-main.fce06d4f.js"
+  },
+  {
+    "revision": "d363978660fa3193bcd6c5c03dc18813",
+    "url": "/static/media/bebidas.d3639786.jpeg"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "69200fc77713e0b4fafa5ac5093cdabf",
+    "url": "/static/media/comidas.69200fc7.jpg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "05999137a91d4f26cd63fdfb3f3d766a",
+    "url": "/static/media/login-wallpaper.05999137.jpg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "0901ed89ff9d90f85ac1611ef9895e2f",
+    "url": "/static/media/notfound.0901ed89.gif"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
