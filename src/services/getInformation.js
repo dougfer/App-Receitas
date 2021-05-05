cd85965e00d611a4ee6391f59b3555696eb338de
Douglas Ferreira <dodofp.df@gmail.com>
2021-05-05 12:03:58 -0300

    Updates

--- src/services/getInformation.js
+++
@@ -1,7 +0,0 @@
-const getInformation = (callback) => {
-  if ((localStorage.getItem('favoriteRecipes') !== null)) {
-    callback(JSON.parse(localStorage.getItem('favoriteRecipes')));
-  }
-};
-
-export default getInformation;
